99e4d6f4d23c9d2aa39e4262e15b37e857d519da
wonjonghwan <woneric05@gmail.com>
2026-03-13 15:19:26 +0900

    수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args){
 
         System.out.println("hello!! My First Java coding!!!!!!!!2222222222");
+        System.out.println("hello!! My First Java coding!!!!!!!!2222222222");
 
     }
 }
